17a69d310a4bbe8ebce7521e39b724ab9c92872f
narmin422 <150496408+narmin422@users.noreply.github.com>
2024-04-29 14:47:53 +0400

    Add files via upload

---
+++ product-main/src/App.jsx
@@ -0,0 +1,18 @@
+
+import'./index.css'
+import {ProductName,Price}  from'./product'
+
+
+function App() {
+ 
+
+  return (
+    <>
+      <ProductName/>
+
+    <Price/>
+    </>
+  )
+}
+
+export default App
---
+++ product-main/src/product.jsx
@@ -0,0 +1,11 @@
+ 
+  
+    export function ProductName(){
+    return <h3>Car</h3>
+   }
+    export function Price (){
+
+    
+    return <p>10000</p>
+   }
+  
